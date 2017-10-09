1e8a72aafabece894e5df03a6cdc06af1885b256
amit98765 <amitkumarmittalakm@gmail.com>
2017-10-10 09:24:39 +1100

    zoom implemented, needs polishing though

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ZoomitComponent } from './zoomit/zoomit.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ZoomitComponent } from './zoomit/zoomit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZoomitComponent
   ],
   imports: [
     BrowserModule
